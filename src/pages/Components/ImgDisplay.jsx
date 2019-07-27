a71f6e2daf6d65e2268041ac8fc3d62f74d58572
Linden Chiu <hchiu87@gmail.com>
2019-07-27 14:22:07 -0700

    current version

--- src/pages/Components/ImgDisplay.jsx
+++ src/pages/Components/ImgDisplay.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import axios from "axios";
 
 class ImgDisplay extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      imgSrc: ''
+    }
 }
